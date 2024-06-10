2546a2d877bfb2ef47b4288bd578eb6aabfd1a16
man25816@stu.kmu.ac.kr <man25816@stu.kmu.ac.kr>
2024-06-10 23:15:42 +0900

    코드 리빌딩 전, 크롤링 완료

--- data_processor.py
+++ data_processor.py
@@ -10,7 +10,7 @@ class DataProcessor:
 
     def set_Ocid(self):
         try:
-            day = mapleCurl.date()
+            day = self.date()
 
             table = self.maindata.get_data()
 
@@ -19,7 +19,7 @@ class DataProcessor:
                 print(character_name)
 
                 # OCID를 크롤링하여 가져오기
-                response_data = mapleCurl.get_character_ocid(character_name, self.maindata.headers)
+                response_data = self.get_character_ocid(character_name, self.maindata.headers)
                 ocid_value = response_data.get('ocid')
 
                 # 테이블에 다시 저장
@@ -104,250 +104,6 @@ class DataProcessor:
             print("오류 발생:", e)
         return None  # 전투력 정보가 없는 경우 None 반환
 
-    # 장비
-    def set_equip(self):
-        try:
-            day = mapleCurl.date()
-
-
-            table = self.maindata.get_data()
-
-            for character in table:
-                character_name, ocid_value, *rest = character
-                print(character_name)
-
-                # 새로운 OCID로 전투력 업데이트
-                character_equip_data = mapleCurl.get_character_equip(ocid_value, day, self.maindata.headers)
-
-                json_data_str = json.dumps(character_equip_data)
-                '''
-                combat_power = self.extract_combat(json_data_str)
-                if combat_power is not None:
-                    character[2] = combat_power  # 전투력 값을 리스트에 추가
-                    print(character)
-                else:
-                    print("전투력 정보를 찾을 수 없습니다.")
-                    return
-                '''
-
-                if json_data_str is not None:
-                    print(json_data_str)
-                else:
-                    print("장비 정보를 찾을 수 없습니다.")
-                    return
-
-                # 대기(0.2초) 코드 추가 (실제 서비스에서는 필요 없음)
-                time.sleep(0.2)
-                # json 저장
-                manager.JsonDataHandler.save_json(character_equip_data, character_name + 'equip')
-                # self.maindata.save_table_to_json('table')
-
-
-        except json.JSONDecodeError as e:
-            print("JSON 디코딩 오류:", e)
-
-        except requests.RequestException as e:
-            print("요청 오류:", e)
-
-        except Exception as e:
-            print("오류 발생:", e)
-
-    # 심볼
-    def set_symbol(self):
-        try:
-            day = mapleCurl.date()
-
-
-            table = self.maindata.get_data()
-
-            for character in table:
-                character_name, ocid_value, *rest = character
-                print(character_name)
-
-                # 새로운 OCID로 심볼 업데이트
-                character_symbol_data = mapleCurl.get_character_symbol(ocid_value, day, self.maindata.headers)
-
-                json_data_str = json.dumps(character_symbol_data)
-
-                if json_data_str is not None:
-                    print(json_data_str)
-                else:
-                    print("심볼 정보를 찾을 수 없습니다.")
-                    return
-
-                # 대기(0.2초) 코드 추가 (실제 서비스에서는 필요 없음)
-                time.sleep(0.2)
-                # json 저장
-                manager.JsonDataHandler.save_json(character_symbol_data, character_name + 'symbol')
-                # elf.maindata.save_table_to_json('table')
-
-
-        except json.JSONDecodeError as e:
-            print("JSON 디코딩 오류:", e)
-
-        except requests.RequestException as e:
-            print("요청 오류:", e)
-
-        except Exception as e:
-            print("오류 발생:", e)
-
-    # 장비 세트
-    def set_setting(self):
-        try:
-            day = mapleCurl.date()
-
-
-            table = self.maindata.get_data()
-
-            for character in table:
-                character_name, ocid_value, *rest = character
-                print(character_name)
-
-                # 새로운 OCID로 장비 세트 업데이트
-                character_setting_data = mapleCurl.get_character_set(ocid_value, day, self.maindata.headers)
-
-                json_data_str = json.dumps(character_setting_data)
-
-                if json_data_str is not None:
-                    print(json_data_str)
-                else:
-                    print("심볼 정보를 찾을 수 없습니다.")
-                    return
-
-                # 대기(0.2초) 코드 추가 (실제 서비스에서는 필요 없음)
-                time.sleep(0.2)
-                # json 저장
-                manager.JsonDataHandler.save_json(character_setting_data, character_name + 'setting')
-                # elf.maindata.save_table_to_json('table')
-
-
-        except json.JSONDecodeError as e:
-            print("JSON 디코딩 오류:", e)
-
-        except requests.RequestException as e:
-            print("요청 오류:", e)
-
-        except Exception as e:
-            print("오류 발생:", e)
-
-    # 0차 스킬
-    def set_setting(self):
-        try:
-            day = mapleCurl.date()
-
-
-            table = self.maindata.get_data()
-
-            for character in table:
-                character_name, ocid_value, *rest = character
-                print(character_name)
-
-                # 새로운 OCID로 장비 세트 업데이트
-                character_skill_data = mapleCurl.get_character_skill(ocid_value, day, self.maindata.headers, 0)
-
-                json_data_str = json.dumps(character_skill_data)
-
-                if json_data_str is not None:
-                    print(json_data_str)
-                else:
-                    print("심볼 정보를 찾을 수 없습니다.")
-                    return
-
-                # 대기(0.2초) 코드 추가 (실제 서비스에서는 필요 없음)
-                time.sleep(0.2)
-                # json 저장
-                manager.JsonDataHandler.save_json(character_skill_data, character_name + 'skill')
-                # elf.maindata.save_table_to_json('table')
-
-
-        except json.JSONDecodeError as e:
-            print("JSON 디코딩 오류:", e)
-
-        except requests.RequestException as e:
-            print("요청 오류:", e)
-
-        except Exception as e:
-            print("오류 발생:", e)
-
-
-    # 헥사 코어
-    def set_hexaCore(self):
-        try:
-            day = mapleCurl.date()
-
-
-            table = self.maindata.get_data()
-
-            for character in table:
-                character_name, ocid_value, *rest = character
-                print(character_name)
-
-                # 새로운 OCID로 장비 세트 업데이트
-                character_hexaCore_data = mapleCurl.get_character_hexaCore(ocid_value, day, self.maindata.headers)
-
-                json_data_str = json.dumps(character_hexaCore_data)
-
-                if json_data_str is not None:
-                    print(json_data_str)
-                else:
-                    print("심볼 정보를 찾을 수 없습니다.")
-                    return
-
-                # 대기(0.2초) 코드 추가 (실제 서비스에서는 필요 없음)
-                time.sleep(0.2)
-                # json 저장
-                manager.JsonDataHandler.save_json(character_hexaCore_data, character_name + 'hexaCore')
-                # elf.maindata.save_table_to_json('table')
-
-
-        except json.JSONDecodeError as e:
-            print("JSON 디코딩 오류:", e)
-
-        except requests.RequestException as e:
-            print("요청 오류:", e)
-
-        except Exception as e:
-            print("오류 발생:", e)
-
-    # 유니온 개인정보
-    def set_union(self):
-        try:
-            day = mapleCurl.date()
-
-
-            table = self.maindata.get_data()
-
-            for character in table:
-                character_name, ocid_value, *rest = character
-                print(character_name)
-
-                # 새로운 OCID로 장비 세트 업데이트
-                character_union_data = mapleCurl.get_character_union(ocid_value, day, self.maindata.headers)
-
-                json_data_str = json.dumps(character_union_data)
-
-                if json_data_str is not None:
-                    print(json_data_str)
-                else:
-                    print("심볼 정보를 찾을 수 없습니다.")
-                    return
-
-                # 대기(0.2초) 코드 추가 (실제 서비스에서는 필요 없음)
-                time.sleep(0.2)
-                # json 저장
-                manager.JsonDataHandler.save_json(character_union_data, character_name + 'union')
-                # elf.maindata.save_table_to_json('table')
-
-
-        except json.JSONDecodeError as e:
-            print("JSON 디코딩 오류:", e)
-
-        except requests.RequestException as e:
-            print("요청 오류:", e)
-
-        except Exception as e:
-            print("오류 발생:", e)
-
 
     # 결과 계산 및 출력(분리 예정)
     def result_print(self):
@@ -384,6 +140,7 @@ class DataProcessor:
 
 # 모든 단계 실행
 maple_manager = DataProcessor()
-# maple_manager.set_Ocid()
-# maple_manager.set_stat()
+
+
+# 결과 출력
 # maple_manager.result_print()
--- mapleCurl.py
+++ mapleCurl.py
@@ -1,93 +1,455 @@
 import requests
 import json
+import time
 from datetime import datetime, timedelta
-
-def date():
-    """
-    Get the previous date.
-    """
-    day = datetime.now()
-    day -= timedelta(days=1)
-    day = day.date()
-    return day
-
-# 캐릭터의 Ocid 크롤링
-def get_character_ocid(character_name, headers):
-    """
-    Retrieve OCID for a given character name.
-    """
-    urlString = "https://open.api.nexon.com/maplestory/v1/id?character_name=" + character_name
-    response = requests.get(urlString, headers=headers)
-    return response.json()
+import manager
 
 
-# 캐릭터의 스텟 크롤링
-def get_character_stat(ocid_value, day, headers):
-    """
-    Retrieve character stats for a given OCID and date.
-    """
-    urlString = "https://open.api.nexon.com/maplestory/v1/character/stat?ocid=" + ocid_value + "&date=" + str(day)
-    response = requests.get(urlString, headers=headers)
-    return response.json()
+class Curl:
+    def __init__(self):
+        self.maindata = manager.MainData()
+
+    def date():
+        """
+        Get the previous date.
+        """
+        day = datetime.now()
+        day -= timedelta(days=1)
+        day = day.date()
+        return day
+
+    # 캐릭터의 Ocid 크롤링
+    def get_character_ocid(character_name, headers):
+        urlString = "https://open.api.nexon.com/maplestory/v1/id?character_name=" + character_name
+        response = requests.get(urlString, headers=headers)
+        return response.json()
+
+    # 캐릭터의 스텟 크롤링
+    def get_character_stat(ocid_value, day, headers):
+        urlString = "https://open.api.nexon.com/maplestory/v1/character/stat?ocid=" + ocid_value + "&date=" + str(day)
+        response = requests.get(urlString, headers=headers)
+        return response.json()
+
+    # 캐릭터의 기본 정보 크롤링
+    def get_character_basicInfo(ocid_value, day, headers):
+        urlString = "https://open.api.nexon.com/maplestory/v1/character/basic?ocid=" + ocid_value + "&date=" + str(day)
+        response = requests.get(urlString, headers=headers)
+        return response.json()
+
+    # 캐릭터의 장비 크롤링
+    # 제네시스 무기 간접 체크 가능
+    def get_character_equip(ocid_value, day, headers):
+        urlString = "https://open.api.nexon.com/maplestory/v1/character/item-equipment?ocid=" + ocid_value + "&date=" + str(day)
+        response = requests.get(urlString, headers=headers)
+        return response.json()
+
+    # 캐릭터의 심볼 크롤링
+    # 어센틱 심볼 체크 가능
+    def get_character_symbol(ocid_value, day, headers):
+        urlString = "https://open.api.nexon.com/maplestory/v1/character/symbol-equipment?ocid=" + ocid_value + "&date=" + str(day)
+        response = requests.get(urlString, headers=headers)
+        return response.json()
+
+    # 캐릭터의 장비 세트(캐시 장비 제외) 크롤링
+    def get_character_set(ocid_value, day, headers):
+        urlString = "https://open.api.nexon.com/maplestory/v1/character/set-effect?ocid=" + ocid_value + "&date=" + str(day)
+        response = requests.get(urlString, headers=headers)
+        return response.json()
+
+    # 캐릭터의 스킬 크롤링
+    def get_character_skill(ocid_value, day, headers, num):
+        urlString = "https://open.api.nexon.com/maplestory/v1/character/skill?ocid=" + ocid_value + "&date=" + str(day) + "&character_skill_grade=" + num
+        response = requests.get(urlString, headers=headers)
+        return response.json()
+
+    # 캐릭터의 헥사 코어 크롤링
+    def get_character_hexaCore(ocid_value, day, headers):
+        urlString = "https://open.api.nexon.com/maplestory/v1/character/hexamatrix?ocid=" + ocid_value + "&date=" + str(day)
+        response = requests.get(urlString, headers=headers)
+        return response.json()
+
+    # 캐릭터의 유니온 정보 크롤링
+    def get_character_union(ocid_value, day, headers):
+        urlString = "https://open.api.nexon.com/maplestory/v1/user/union?ocid=" + ocid_value + "&date=" + str(day)
+        response = requests.get(urlString, headers=headers)
+        return response.json()
+
+    # 아레의 함수들은 전 캐릭을 순회하며 크롤링하는 함수들
+
+    # 전직업 Ocid
+    def set_Ocid(self):
+        try:
+            day = self.date()
+
+            table = self.maindata.get_data()
+
+            for character in table:
+                character_name, ocid_value, *rest = character
+                print(character_name)
+
+                # OCID를 크롤링하여 가져오기
+                response_data = self.get_character_ocid(character_name, self.maindata.headers)
+                ocid_value = response_data.get('ocid')
+
+                # 테이블에 다시 저장
+                if ocid_value is not None:
+                    print(character)
+                    print(f"캐릭터 {character_name}의 OCID가 업데이트되었습니다.")
+                    print(ocid_value)
+                else:
+                    print("OCID를 찾을 수 없습니다.")
+                    return
+                # 대기(0.2초) 코드 추가 (실제 서비스에서는 필요 없음)
+                time.sleep(0.2)
+
+        except json.JSONDecodeError as e:
+            print("JSON 디코딩 오류:", e)
+
+        except requests.RequestException as e:
+            print("요청 오류:", e)
+
+        except Exception as e:
+            print("오류 발생:", e)
+
+    # 스텟
+    def set_stat(self):
+        try:
+            day = self.date()
 
 
-# 캐릭터의 장비 크롤링
-# 제네시스 무기 간접 체크 가능
-def get_character_equip(ocid_value, day, headers):
-    """
-    Retrieve character stats for a given OCID and date.
-    """
-    urlString = "https://open.api.nexon.com/maplestory/v1/character/item-equipment?ocid=" + ocid_value + "&date=" + str(day)
-    response = requests.get(urlString, headers=headers)
-    return response.json()
+            table = self.maindata.get_data()
 
-# 캐릭터의 심볼 크롤링
-# 어센틱 심볼 체크 가능
-def get_character_symbol(ocid_value, day, headers):
-    """
-    Retrieve character stats for a given OCID and date.
-    """
-    urlString = "https://open.api.nexon.com/maplestory/v1/character/symbol-equipment?ocid=" + ocid_value + "&date=" + str(day)
-    response = requests.get(urlString, headers=headers)
-    return response.json()
+            for character in table:
+                character_name, ocid_value, *rest = character
+                print(character_name)
 
-# 캐릭터의 장비 세트(캐시 장비 제외) 크롤링
-def get_character_set(ocid_value, day, headers):
-    """
-    Retrieve character stats for a given OCID and date.
-    """
-    urlString = "https://open.api.nexon.com/maplestory/v1/character/set-effect?ocid=" + ocid_value + "&date=" + str(day)
-    response = requests.get(urlString, headers=headers)
-    return response.json()
+                # 새로운 OCID로 전투력 업데이트
+                character_stat_data = self.get_character_stat(ocid_value, day, self.maindata.headers)
 
-# 캐릭터의 스킬 크롤링
-def get_character_skill(ocid_value, day, headers, num):
-    """
-    Retrieve character stats for a given OCID and date.
-    """
-    urlString = "https://open.api.nexon.com/maplestory/v1/character/skill?ocid=" + ocid_value + "&date=" + str(day) + "&character_skill_grade=" + num
-    response = requests.get(urlString, headers=headers)
-    return response.json()
+                json_data_str = json.dumps(character_stat_data)
 
-# 캐릭터의 헥사 코어 크롤링
-def get_character_hexaCore(ocid_value, day, headers):
-    """
-    Retrieve character stats for a given OCID and date.
-    """
-    urlString = "https://open.api.nexon.com/maplestory/v1/character/hexamatrix?ocid=" + ocid_value + "&date=" + str(day)
-    response = requests.get(urlString, headers=headers)
-    return response.json()
+                if json_data_str is not None:
+                    print(json_data_str)
+                else:
+                    print("전투력 정보를 찾을 수 없습니다.")
+                    return
 
-# 캐릭터의 유니온 정보 크롤링
-def get_character_union(ocid_value, day, headers):
-    """
-    Retrieve character stats for a given OCID and date.
-    """
-    urlString = "https://open.api.nexon.com/maplestory/v1/user/union?ocid=" + ocid_value + "&date=" + str(day)
-    response = requests.get(urlString, headers=headers)
-    return response.json()
+                # 대기(0.2초) 코드 추가 (실제 서비스에서는 필요 없음)
+                time.sleep(0.2)
+                # json 저장
+                manager.JsonDataHandler.save_json(character_stat_data, character_name + 'stat')
 
-'''
-0차 스킬의 해방 스킬로 해방 여부 판단 가능
 
-'''
+        except json.JSONDecodeError as e:
+            print("JSON 디코딩 오류:", e)
+
+        except requests.RequestException as e:
+            print("요청 오류:", e)
+
+        except Exception as e:
+            print("오류 발생:", e)
+
+    # 기본 정보
+    def set_basicInfo(self):
+        try:
+            day = self.date()
+
+
+            table = self.maindata.get_data()
+
+            for character in table:
+                character_name, ocid_value, *rest = character
+                print(character_name)
+
+                # 새로운 OCID로 전투력 업데이트
+                character_basicInfo_data = self.get_character_basicInfo(ocid_value, day, self.maindata.headers)
+
+                json_data_str = json.dumps(character_basicInfo_data)
+
+                if json_data_str is not None:
+                    print(json_data_str)
+                else:
+                    print("장비 정보를 찾을 수 없습니다.")
+                    return
+
+                # 대기(0.2초) 코드 추가 (실제 서비스에서는 필요 없음)
+                time.sleep(0.2)
+                # json 저장
+                manager.JsonDataHandler.save_json(character_basicInfo_data, character_name + 'basicInfo')
+
+        except json.JSONDecodeError as e:
+            print("JSON 디코딩 오류:", e)
+
+        except requests.RequestException as e:
+            print("요청 오류:", e)
+
+        except Exception as e:
+            print("오류 발생:", e)
+
+    # 장비
+    def set_equip(self):
+        try:
+            day = self.date()
+
+
+            table = self.maindata.get_data()
+
+            for character in table:
+                character_name, ocid_value, *rest = character
+                print(character_name)
+
+                # 새로운 OCID로 전투력 업데이트
+                character_equip_data = self.get_character_equip(ocid_value, day, self.maindata.headers)
+
+                json_data_str = json.dumps(character_equip_data)
+
+                if json_data_str is not None:
+                    print(json_data_str)
+                else:
+                    print("장비 정보를 찾을 수 없습니다.")
+                    return
+
+                # 대기(0.2초) 코드 추가 (실제 서비스에서는 필요 없음)
+                time.sleep(0.2)
+                # json 저장
+                manager.JsonDataHandler.save_json(character_equip_data, character_name + 'equip')
+
+        except json.JSONDecodeError as e:
+            print("JSON 디코딩 오류:", e)
+
+        except requests.RequestException as e:
+            print("요청 오류:", e)
+
+        except Exception as e:
+            print("오류 발생:", e)
+
+    # 심볼
+    def set_symbol(self):
+        try:
+            day = self.date()
+
+
+            table = self.maindata.get_data()
+
+            for character in table:
+                character_name, ocid_value, *rest = character
+                print(character_name)
+
+                # 새로운 OCID로 심볼 업데이트
+                character_symbol_data = self.get_character_symbol(ocid_value, day, self.maindata.headers)
+
+                json_data_str = json.dumps(character_symbol_data)
+
+                if json_data_str is not None:
+                    print(json_data_str)
+                else:
+                    print("심볼 정보를 찾을 수 없습니다.")
+                    return
+
+                # 대기(0.2초) 코드 추가 (실제 서비스에서는 필요 없음)
+                time.sleep(0.2)
+                # json 저장
+                manager.JsonDataHandler.save_json(character_symbol_data, character_name + 'symbol')
+
+        except json.JSONDecodeError as e:
+            print("JSON 디코딩 오류:", e)
+
+        except requests.RequestException as e:
+            print("요청 오류:", e)
+
+        except Exception as e:
+            print("오류 발생:", e)
+
+    # 장비 세트
+    def set_setting(self):
+        try:
+            day = self.date()
+
+
+            table = self.maindata.get_data()
+
+            for character in table:
+                character_name, ocid_value, *rest = character
+                print(character_name)
+
+                # 새로운 OCID로 장비 세트 업데이트
+                character_setting_data = self.get_character_set(ocid_value, day, self.maindata.headers)
+
+                json_data_str = json.dumps(character_setting_data)
+
+                if json_data_str is not None:
+                    print(json_data_str)
+                else:
+                    print("장비 세트 정보를 찾을 수 없습니다.")
+                    return
+
+                # 대기(0.2초) 코드 추가 (실제 서비스에서는 필요 없음)
+                time.sleep(0.2)
+                # json 저장
+                manager.JsonDataHandler.save_json(character_setting_data, character_name + 'setting')
+
+        except json.JSONDecodeError as e:
+            print("JSON 디코딩 오류:", e)
+
+        except requests.RequestException as e:
+            print("요청 오류:", e)
+
+        except Exception as e:
+            print("오류 발생:", e)
+
+    # 0차 스킬
+    def set_skill(self, num):
+        try:
+            day = self.date()
+
+            number = num
+
+
+            table = self.maindata.get_data()
+
+            for character in table:
+                character_name, ocid_value, *rest = character
+                print(character_name)
+
+                # 새로운 OCID로 장비 세트 업데이트
+                character_skill_data = self.get_character_skill(ocid_value, day, self.maindata.headers, number)
+
+                json_data_str = json.dumps(character_skill_data)
+
+                if json_data_str is not None:
+                    print(json_data_str)
+                else:
+                    print("스킬 정보를 찾을 수 없습니다.")
+                    return
+
+                # 대기(0.2초) 코드 추가 (실제 서비스에서는 필요 없음)
+                time.sleep(0.2)
+                # json 저장
+                manager.JsonDataHandler.save_json(character_skill_data, character_name + 'skill')
+
+        except json.JSONDecodeError as e:
+            print("JSON 디코딩 오류:", e)
+
+        except requests.RequestException as e:
+            print("요청 오류:", e)
+
+        except Exception as e:
+            print("오류 발생:", e)
+
+
+    # 헥사 코어
+    def set_hexaCore(self):
+        try:
+            day = self.date()
+
+
+            table = self.maindata.get_data()
+
+            for character in table:
+                character_name, ocid_value, *rest = character
+                print(character_name)
+
+                # 새로운 OCID로 장비 세트 업데이트
+                character_hexaCore_data = self.get_character_hexaCore(ocid_value, day, self.maindata.headers)
+
+                json_data_str = json.dumps(character_hexaCore_data)
+
+                if json_data_str is not None:
+                    print(json_data_str)
+                else:
+                    print("핵사코어 정보를 찾을 수 없습니다.")
+                    return
+
+                # 대기(0.2초) 코드 추가 (실제 서비스에서는 필요 없음)
+                time.sleep(0.2)
+                # json 저장
+                manager.JsonDataHandler.save_json(character_hexaCore_data, character_name + 'hexaCore')
+
+        except json.JSONDecodeError as e:
+            print("JSON 디코딩 오류:", e)
+
+        except requests.RequestException as e:
+            print("요청 오류:", e)
+
+        except Exception as e:
+            print("오류 발생:", e)
+
+    # 유니온 개인정보
+    def set_union(self):
+        try:
+            day = self.date()
+
+            table = self.maindata.get_data()
+
+            character_name = "나초봄지"
+            ocid_value = "57933d3d5733ed493653cd4d3d9738e8"
+            print(character_name)
+
+            # 새로운 OCID로 장비 세트 업데이트
+            character_union_data = self.get_character_union(ocid_value, day, self.maindata.headers)
+
+            json_data_str = json.dumps(character_union_data)
+
+            if json_data_str is not None:
+                print(json_data_str)
+            else:
+                print("유니온 정보를 찾을 수 없습니다.")
+                return
+
+            # 대기(0.2초) 코드 추가 (실제 서비스에서는 필요 없음)
+            time.sleep(0.2)
+            # json 저장
+            manager.JsonDataHandler.save_json(character_union_data, 'union')
+
+        except json.JSONDecodeError as e:
+            print("JSON 디코딩 오류:", e)
+
+        except requests.RequestException as e:
+            print("요청 오류:", e)
+
+        except Exception as e:
+            print("오류 발생:", e)
+
+    '''
+    0차 스킬의 해방 스킬로 해방 여부 판단 가능
+
+    '''
+
+
+# 모든 단계 실행
+maple_curl = Curl()
+
+# 캐릭터별 Ocid 크롤링
+# maple_curl.set_Ocid()
+
+# 대기(1초) 코드 추가 (실제 서비스에서는 필요 없음), 캐릭별 기본 정보 크롤링
+# time.sleep(1)
+# maple_curl.set_basicInfo()
+
+# 대기(1초) 코드 추가 (실제 서비스에서는 필요 없음), 캐릭별 기본 스텟 크롤링
+# time.sleep(1)
+# maple_curl.set_stat()
+
+# 대기(1초) 코드 추가 (실제 서비스에서는 필요 없음), 캐릭별 장비 크롤링
+# time.sleep(1)
+# maple_curl.set_equip()
+
+# 대기(1초) 코드 추가 (실제 서비스에서는 필요 없음), 캐릭별 헥사 코어 크롤링
+# time.sleep(1)
+# maple_curl.set_hexaCore()
+
+# 대기(1초) 코드 추가 (실제 서비스에서는 필요 없음), 캐릭별 심볼 크롤링
+# time.sleep(1)
+# maple_curl.set_symbol()
+
+# 대기(1초) 코드 추가 (실제 서비스에서는 필요 없음), 캐릭별 스킬 크롤링
+# time.sleep(1)
+# maple_curl.set_skill(0)
+
+# 대기(1초) 코드 추가 (실제 서비스에서는 필요 없음), 캐릭별 장비 세팅 크롤링
+# time.sleep(1)
+# maple_curl.set_setting()
+
+# 대기(1초) 코드 추가 (실제 서비스에서는 필요 없음), 캐릭별 심볼 크롤링
+# time.sleep(1)
+# maple_curl.set_union()
